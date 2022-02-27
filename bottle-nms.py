c038d07d4bbe365a86c3e7faf281cf3233585c31
7HR4IZ3 <90985774+7HR4IZ3@users.noreply.github.com>
2022-02-27 09:51:55 +0100

    Update and rename bottle-nms.py to router.py

--- bottle-nms.py
+++
@@ -1,8 +0,0 @@
-class namespace:
-    def __init__(self, base, route):
-        self.base_url = base
-        self.base_route = route
-        
-    def route(self, url, *args, **kwargs):
-        return self.base_route(self.base_url+url, *args, *kwargs)
-    
